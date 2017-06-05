6a61632a31183b22bb5cf0579682d6420838c651
Dale Jung <dale@dalejung.com>
2017-06-04 23:23:13 -0400

    quick stub for debugging

--- geeknote/log.py
+++ geeknote/log.py
@@ -8,3 +8,14 @@ if config.DEBUG:
     logging.basicConfig(format="%(filename)s %(funcName)s %(lineno)d : %(message)s", level=logging.DEBUG)
 else:
     logging.basicConfig(format="%(asctime)-15s %(module)s %(funcName)s %(lineno)d : %(message)s", filename=config.ERROR_LOG)
+
+
+if False:
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
